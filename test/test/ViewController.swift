588fd94ff07e8eb2a6f5478a7ff7e5c2d7d17683
Jack-Livingston <31826386+Jack-Livingston@users.noreply.github.com>
2017-11-30 13:56:57 -0500

    Removed Exercise

---
+++ test/test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Jack Livingston on 11/23/17.
+//  Copyright © 2017 jack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
